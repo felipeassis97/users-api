4dc73b421cedda1706b6acf4793e608b71144d56
felipeassis97 <felipeassis97@gmail.com>
2024-06-01 14:02:34 +1000

    feat: add MongoCreateUserRepository

---
+++ src/controllers/create-user/protocols.ts
@@ -0,0 +1,12 @@
+import { User } from "../../models/users";
+
+export interface ICreateUserRepository {
+  createUser(params: CreateUserParams): Promise<User>;
+}
+
+export interface CreateUserParams {
+  firstname: string;
+  lastname: string;
+  email: string;
+  password: string;
+}
