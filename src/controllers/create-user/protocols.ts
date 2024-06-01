26c42289a856ade6ebce102a8536a739683319bf
felipeassis97 <felipeassis97@gmail.com>
2024-06-01 14:25:20 +1000

    feat: Save user to mongo

--- src/controllers/create-user/protocols.ts
+++ src/controllers/create-user/protocols.ts
@@ -1,7 +1,14 @@
 import { User } from "../../models/users";
+import { HttpRequest, HttpResponse } from "../protocols";
 
 export interface ICreateUserRepository {
   createUser(params: CreateUserParams): Promise<User>;
+}
+
+export interface ICreateUserController {
+  handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User>>;
 }
 
 export interface CreateUserParams {
--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,13 @@ import { config } from "dotenv";
 import { GetUsersController } from "./controllers/get-users/get-users";
 import { MongoGetUsersRepository } from "./repositories/get-users/mongo-get-users";
 import { MongoClient } from "./database/mongo";
+import { MongoCreateUserRepository } from "./repositories/create-user/mongo-create-user";
+import { CreateUserController } from "./controllers/create-user/create-user";
 
 const main = async () => {
   config();
   const app = express();
+  app.use(express.json());
   await MongoClient.connect();
 
   //Routes
@@ -14,6 +17,14 @@ const main = async () => {
     const repository = new MongoGetUsersRepository();
     const controller = new GetUsersController(repository);
     const { body, statusCode } = await controller.handle();
+
+    res.send(body).status(statusCode);
+  });
+
+  app.post("/user", async (req, res) => {
+    const repository = new MongoCreateUserRepository();
+    const controller = new CreateUserController(repository);
+    const { body, statusCode } = await controller.handle({ body: req.body });
 
     res.send(body).status(statusCode);
   });
